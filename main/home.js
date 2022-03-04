cb3f1bde3cfc8a735906853a1acf6db73d01d65a
Dallin Breen <dallinbreen624@gmail.com>
2022-03-03 17:45:38 -0700

    added create character div

--- main/home.js
+++ main/home.js
@@ -1,13 +1,21 @@
+const infoBox = document.getElementById("character-info");
+const createBtn = document.getElementById("create-character");
+const logoutBtn = document.getElementById("logout");
 
+const addNewCharacter = (e) => {
+  e.preventDefault();
+  infoBox.innerHTML = `<form id="create-character-form">
+  <label for="charcter-creation-input">Enter Character Name</label>
+  <input type="text" name="charcter-creation-input" id="charcter-creation-input" required>
+  <input type="submit" name="charcter-creation-submit" id="charcter-creation-submit">
+</form>`;
+};
 
-
-
-
-
+createBtn.addEventListener("click", addNewCharacter);
 
 const logout = () => {
-    window.localStorage.clear();
-    window.location.href = "./login.html";
-}
+  window.localStorage.clear();
+  window.location.href = "./login.html";
+};
 
-const logoutBtn = document.getElementById('logout').addEventListener('click', logout);
+logoutBtn.addEventListener("click", logout);
